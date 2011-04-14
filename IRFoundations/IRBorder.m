6b84a6f8aed212af262416fd9c10a4a219a1255a
Evadne Wu <ev@monoceroi.com>
2011-04-14 09:54:24 +0800

    IRBorder <NSCoding>

--- IRFoundations/IRBorder.m
+++ IRFoundations/IRBorder.m
@@ -28,6 +28,29 @@
 
 }
 
+- (id) initWithCoder:(NSCoder *)aDecoder {
+
+	self = [super init];
+	if (!self) return nil;
+
+	[(NSValue *)[aDecoder decodeObjectForKey:@"edge"] getValue:&edge];
+	[(NSValue *)[aDecoder decodeObjectForKey:@"type"] getValue:&type];
+	self.width = [aDecoder decodeFloatForKey:@"width"];
+	self.color = [aDecoder decodeObjectForKey:@"color"];
+		
+	return self;
+
+}
+
+- (void) encodeWithCoder:(NSCoder *)aCoder {
+
+	[aCoder encodeObject:[NSValue valueWithBytes:&edge objCType:@encode(__typeof__(edge))] forKey:@"edge"];
+	[aCoder encodeObject:[NSValue valueWithBytes:&type objCType:@encode(__typeof__(type))] forKey:@"type"];
+	[aCoder encodeFloat:width forKey:@"width"];
+	[aCoder encodeObject:color forKey:@"color"];
+
+}
+
 - (void) dealloc {
 
 	[color release];
