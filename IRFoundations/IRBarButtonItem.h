89c45243502b59c7763a99e1c5907cc98536f837
Evadne Wu <ev@monoceroi.com>
2011-04-16 14:15:50 +0800

    IRBarButtonItem now has conveniences regarding custom back button items

--- IRFoundations/IRBarButtonItem.h
+++ IRFoundations/IRBarButtonItem.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "IRShadow.h"
+#import "IRBorder.h"
 
 @interface IRBarButtonItem : UIBarButtonItem
 
@@ -17,6 +18,10 @@
 - (IBAction) handleCustomButtonAction:(id)sender; // button will be wired to the item, which runs the block
 
 + (id) itemWithSystemItem:(UIBarButtonSystemItem)aSystemItem wiredAction:(void(^)(IRBarButtonItem *senderItem))aBlock;
+
++ (UIImage *) backButtonImageWithTitle:(NSString *)aTitle font:(UIFont *)fontOrNil backgroundColor:(UIColor *)backgroundColorOrNil gradientColors:(NSArray *)backgroundGradientColorsOrNil innerShadow:(IRShadow *)innerShadowOrNil border:(IRBorder *)borderOrNil shadow:(IRShadow *)shadowOrNil; // Typical exhibitionism
+
++ (id) backItemWithTitle:(NSString *)aTitle tintColor:(UIColor *)aColor; // actually *drawn* image
 
 @property (nonatomic, readwrite, copy) void (^block)();
 
--- IRFoundations/IRBarButtonItem.m
+++ IRFoundations/IRBarButtonItem.m
@@ -54,10 +54,173 @@
 
 }
 
++ (id) backItemWithTitle:(NSString *)aTitle tintColor:(UIColor *)aColor {
+
+	IRBarButtonItem *returnedItem = [self itemWithButton:(( ^ {
+	
+		UIButton *returnedButton = [UIButton buttonWithType:UIButtonTypeCustom];
+		
+		[returnedButton setImage:[[self class] backButtonImageWithTitle:aTitle font:nil backgroundColor:nil gradientColors:nil innerShadow:nil border:nil shadow:nil] forState:UIControlStateNormal];
+		
+		[returnedButton setImage:[[self class] backButtonImageWithTitle:aTitle font:nil backgroundColor:nil gradientColors:[NSArray arrayWithObjects:
+		
+		(id)[UIColor colorWithRed:1 green:1 blue:1 alpha:.75].CGColor,
+		(id)[UIColor colorWithRed:.4 green:.4 blue:.4 alpha:.75].CGColor,
+	
+	nil] innerShadow:nil border:nil shadow:nil] forState:UIControlStateHighlighted];
+		
+	//	[returnedButton setAdjustsImageWhenHighlighted:NO];
+		
+		[returnedButton sizeToFit];
+		
+		return returnedButton;
+	
+	})()) wiredAction:nil];
+	
+	return returnedItem;
+
+}
+
 - (IBAction) handleCustomButtonAction:(id)sender {
 
 	NSParameterAssert(self.block);
 	self.block();
+
+}
+
+
+
+
+
++ (UIImage *) backButtonImageWithTitle:(NSString *)aTitle font:(UIFont *)fontOrNil backgroundColor:(UIColor *)backgroundColorOrNil gradientColors:(NSArray *)backgroundGradientColorsOrNil innerShadow:(IRShadow *)innerShadowOrNil border:(IRBorder *)borderOrNil shadow:(IRShadow *)shadowOrNil {
+
+	UIFont *usingFont = fontOrNil ? fontOrNil : [UIFont boldSystemFontOfSize:12.0f];
+	UIColor *buttonBackgroundColor = backgroundColorOrNil ? backgroundColorOrNil : [UIColor colorWithWhite:0.35f alpha:1];
+	IRShadow *buttonShadow = shadowOrNil ? shadowOrNil : [IRShadow shadowWithColor:[UIColor colorWithWhite:1 alpha:0.5] offset:(CGSize){ 0, 1 } spread:0.5];
+	NSArray *buttonGradientColors = backgroundGradientColorsOrNil ? backgroundGradientColorsOrNil : [NSArray arrayWithObjects:
+		
+		(id)[UIColor colorWithRed:1 green:1 blue:1 alpha:1].CGColor,
+		(id)[UIColor colorWithRed:.4 green:.4 blue:.4 alpha:1].CGColor,
+	
+	nil];
+	
+	IRShadow *buttonInnerShadow = [IRShadow shadowWithColor:[UIColor colorWithWhite:0 alpha:0.75] offset:(CGSize){ 0, 1 } spread:2];
+	IRBorder *buttonBorder = [IRBorder borderForEdge:IREdgeNone withType:IRBorderTypeInset width:1.0 color:[UIColor colorWithWhite:0.35 alpha:1]];
+	
+	static const UIEdgeInsets insets = (UIEdgeInsets){ 0, 12, 0, 8 };
+	static const CGFloat cornerRadius = 6;
+	static const CGFloat buttonHeight = 29.0;
+	
+	CGSize titleSize = [aTitle sizeWithFont:usingFont];
+	CGSize finalSize = (CGSize){ titleSize.width + 20 + 8, 44.0f };
+
+	UIGraphicsBeginImageContextWithOptions(finalSize, NO, 0.0);
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	
+	UIBezierPath *bezierPath = (( ^ {
+	
+		UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+
+		[bezierPath moveToPoint:CGPointZero];
+		[bezierPath addLineToPoint:(CGPoint){ insets.left, -1 * (0.5 * buttonHeight) }];
+		[bezierPath addLineToPoint:(CGPoint){ finalSize.width - insets.right - cornerRadius, -1 * (0.5 * buttonHeight) }];
+		
+		[bezierPath addQuadCurveToPoint:(CGPoint){ finalSize.width - insets.right, -1 * (0.5 * buttonHeight - cornerRadius) }
+											 controlPoint:(CGPoint){ finalSize.width - insets.right, -1 * (0.5 * buttonHeight) }];
+		
+		[bezierPath addLineToPoint:(CGPoint){ finalSize.width - insets.right, 1 * (0.5 * buttonHeight - cornerRadius) }];
+		
+		[bezierPath addQuadCurveToPoint:(CGPoint){ finalSize.width - insets.right - cornerRadius, 1 * (0.5 * buttonHeight) }
+											 controlPoint:(CGPoint){ finalSize.width - insets.right, 1 * (0.5 * buttonHeight) }];
+		
+		[bezierPath addLineToPoint:(CGPoint){ insets.left, 1 * (0.5 * buttonHeight) }];
+		[bezierPath addLineToPoint:CGPointZero];
+		
+		[bezierPath applyTransform:CGAffineTransformMakeTranslation(0, -0.5 + 0.5 * finalSize.height)];
+		
+		return bezierPath;
+	
+	})());
+	
+	if (buttonShadow) {
+	
+		CGContextSetShadowWithColor(context, buttonShadow.offset, buttonShadow.spread, buttonShadow.color.CGColor);
+	
+	}
+	
+	CGContextBeginTransparencyLayer(context, NULL);
+	
+	if (buttonBackgroundColor) {
+	
+		CGContextSaveGState(context);
+		CGContextAddPath(context, bezierPath.CGPath);
+		CGContextSetFillColorWithColor(context, buttonBackgroundColor.CGColor);
+		CGContextFillPath(context);
+		CGContextRestoreGState(context);
+	
+	}
+	
+	if (buttonGradientColors) {
+	
+		CGContextSaveGState(context);
+		CGContextAddPath(context, bezierPath.CGPath);
+		CGContextClip(context);
+		CGGradientRef topGradient = CGGradientCreateWithColors(NULL, (CFArrayRef)buttonGradientColors, NULL);
+		CGContextDrawLinearGradient(context, topGradient, CGPointZero, (CGPoint){ 0, floorf(1 * finalSize.height) }, 0);
+		CGGradientRelease(topGradient);
+		CGContextRestoreGState(context);
+	
+	}
+	
+	if (buttonInnerShadow) {
+	
+		CGContextSaveGState(context);
+		CGContextAddPath(context, bezierPath.CGPath);
+		CGContextClip(context);
+		CGContextSetStrokeColorWithColor(context, buttonInnerShadow.color.CGColor);
+		CGContextSetLineWidth(context, buttonInnerShadow.spread);
+		CGContextAddPath(context, bezierPath.CGPath);
+		CGContextSetShadowWithColor(context, buttonInnerShadow.offset, buttonInnerShadow.spread, buttonInnerShadow.color.CGColor);
+		CGContextStrokePath(context);
+		CGContextRestoreGState(context);
+	
+	}
+	
+	if (buttonBorder) {
+	
+		NSParameterAssert(buttonBorder.type == IRBorderTypeInset);
+		
+		CGContextSaveGState(context);
+		CGContextAddPath(context, bezierPath.CGPath);
+		CGContextClip(context);
+		CGContextSetStrokeColorWithColor(context, buttonBorder.color.CGColor);
+		CGContextSetLineWidth(context, buttonBorder.width * 2);
+		CGContextSetBlendMode(context, kCGBlendModeClear); // first clean underlying stuff
+		CGContextAddPath(context, bezierPath.CGPath);
+		CGContextStrokePath(context);
+		CGContextSetBlendMode(context, kCGBlendModeNormal); // then draw over a clear surface
+		CGContextAddPath(context, bezierPath.CGPath);
+		CGContextStrokePath(context);
+		CGContextRestoreGState(context);
+	
+	}
+	
+	if (aTitle) {
+	
+		CGContextSaveGState(context);
+		CGContextSetShadowWithColor(context, (CGSize){ 0, 1 }, 0, [UIColor colorWithWhite:1 alpha:0.5].CGColor);
+		CGContextSetFillColorWithColor(context, [UIColor colorWithWhite:0.2 alpha:1].CGColor);
+		CGRect titleRect = (CGRect){ (CGPoint){ insets.left + 2, floorf(0.5 * (finalSize.height - titleSize.height)) }, finalSize };
+		[aTitle drawInRect:titleRect withFont:usingFont];
+		CGContextRestoreGState(context);
+	
+	}
+	
+	CGContextEndTransparencyLayer(context);
+	
+	UIImage *returnedImage = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	return returnedImage;	
 
 }
 
