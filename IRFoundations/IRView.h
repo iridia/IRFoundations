59af5d951ac06603dcc3627044c3ec68fb37a31a
Evadne Wu <ev@monoceroi.com>
2011-10-14 01:28:21 +0800

    initial ideas for injecting dynamism into UIView for some UILayoutContainerView work related to custom navigation bar / tool bar framing

--- IRFoundations/IRView.h
+++ IRFoundations/IRView.h
@@ -1,13 +1,32 @@
 //
 //  IRView.h
-//  Milk
+//  IRFoundations
 //
 //  Created by Evadne Wu on 1/6/11.
 //  Copyright 2011 Iridia Productions. All rights reserved.
 //
 
+#import <objc/runtime.h>
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
+
+
+extern NSString * const kUIView_IRAdditions_onLayoutSubviews;
+extern NSString * const kUIView_IRAdditions_onDrawRect;
+extern NSString * const kUIView_IRAdditions_onDrawLayerInContext;
+
+@interface UIView (IRAdditions)
+
+//	These additions, when invoked, will attempt to dynamically create subclasses of the receiver’s class, and swizzle the receiver’s class to the dynmically created subclass.
+
+//	If possible, directly use an IRView, since it will provide a very clean implementation without any dynamically created and injected class hierarchies
+
+@property (nonatomic, readwrite, copy) void (^onLayoutSubviews)(UIView *self);
+@property (nonatomic, readwrite, copy) void (^onDrawRect)(UIView *self, CGRect drawnRect);
+@property (nonatomic, readwrite, copy) void (^onDrawLayerInContext)(UIView *self, CALayer *aLayer, CGContextRef aContext);
+
+@end
+
 
 @interface IRView : UIView
 
