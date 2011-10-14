9330d98052a9062bdcd4c23555095fbaedd367f8
Evadne Wu <ev@monoceroi.com>
2011-10-14 17:10:53 +0800

    IRView and UIView+IRAdditions fixed up so there is no longer a stray category; other niceties

--- IRFoundations/IRView.h
+++ IRFoundations/IRView.h
@@ -11,22 +11,6 @@
 #import <QuartzCore/QuartzCore.h>
 
 
-extern NSString * const kUIView_IRAdditions_onLayoutSubviews;
-extern NSString * const kUIView_IRAdditions_onDrawRect;
-extern NSString * const kUIView_IRAdditions_onDrawLayerInContext;
-
-@interface UIView (IRAdditions)
-
-//	These additions, when invoked, will attempt to dynamically create subclasses of the receiver’s class, and swizzle the receiver’s class to the dynmically created subclass.
-
-//	If possible, directly use an IRView, since it will provide a very clean implementation without any dynamically created and injected class hierarchies
-
-@property (nonatomic, readwrite, copy) void (^onLayoutSubviews)(UIView *self);
-@property (nonatomic, readwrite, copy) void (^onDrawRect)(UIView *self, CGRect drawnRect);
-@property (nonatomic, readwrite, copy) void (^onDrawLayerInContext)(UIView *self, CALayer *aLayer, CGContextRef aContext);
-
-@end
-
 
 @interface IRView : UIView
 
--- IRFoundations/IRView.m
+++ IRFoundations/IRView.m
@@ -9,54 +9,7 @@
 #import "IRView.h"
 
 
-NSString * const kUIView_IRAdditions_onLayoutSubviews = @"UIView_IRAdditions_onLayoutSubviews";
-NSString * const kUIView_IRAdditions_onDrawRect = @"UIView_IRAdditions_onDrawRect";
-NSString * const kUIView_IRAdditions_onDrawLayerInContext = @"UIView_IRAdditions_onDrawLayerInContext";
-
-
-@implementation UIView (IRAdditions)
-
-- (void) setOnLayoutSubviews:(void (^)(UIView *))newOnLayoutSubviews {
-
-	objc_setAssociatedObject(self, &kUIView_IRAdditions_onLayoutSubviews, newOnLayoutSubviews, OBJC_ASSOCIATION_COPY_NONATOMIC);
-
-}
-
-- (void) setOnDrawRect:(void (^)(UIView *, CGRect))newOnDrawRect {
-
-	objc_setAssociatedObject(self, &kUIView_IRAdditions_onDrawRect, newOnDrawRect, OBJC_ASSOCIATION_COPY_NONATOMIC);
-
-}
-
-- (void) setOnDrawLayerInContext:(void (^)(UIView *, CALayer *, CGContextRef))newOnDrawLayerInContext {
-
-	objc_setAssociatedObject(self, &kUIView_IRAdditions_onDrawLayerInContext, newOnDrawLayerInContext, OBJC_ASSOCIATION_COPY_NONATOMIC);
-
-}
-
-- (void(^)(UIView *)) onLayoutSubviews {
-
-	return objc_getAssociatedObject(self, &kUIView_IRAdditions_onLayoutSubviews);
-
-}
-
-- (void(^)(UIView *, CGRect)) onDrawRect {
-
-	return objc_getAssociatedObject(self, &kUIView_IRAdditions_onDrawRect);
-
-}
-
-- (void(^)(UIView *, CALayer *, CGContextRef)) onDrawLayerInContext {
-
-	return objc_getAssociatedObject(self, &kUIView_IRAdditions_onDrawLayerInContext);
-
-}
-
-@end
-
-
 @implementation IRView
-
 @synthesize onDrawRect;
 
 - (void) drawRect:(CGRect)rect {
@@ -68,11 +21,9 @@ NSString * const kUIView_IRAdditions_onDrawLayerInContext = @"UIView_IRAdditions
 
 - (void) dealloc {
 
-	self.onDrawRect = nil;
-
+	[onDrawRect release];
 	[super dealloc];
 
 }
-
 
 @end
