065f5b6257a067d8dee460e8a356880492460fdf
Evadne Wu <ev@monoceroi.com>
2011-10-23 19:47:55 +0800

    AssetsLibrary additions; -[ALAssetRepresentation irImage]

---
+++ IRFoundations/AssetsLibrary+IRAdditions.h
@@ -0,0 +1,14 @@
+//
+//  AssetsLibrary+IRAdditions.h
+//  IRFoundations
+//
+//  Created by Evadne Wu on 10/23/11.
+//  Copyright (c) 2011 Iridia Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+
+#import "ALAssetRepresentation+IRAdditions.h"
+
+extern UIImageOrientation irUIImageOrientationFromAssetOrientation (ALAssetOrientation anOrientation);
