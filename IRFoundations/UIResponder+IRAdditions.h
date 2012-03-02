3d8ba7c6face4830b0d46618039857c509376e6d
Evadne Wu <ev@monoceroi.com>
2012-03-03 00:55:26 +0800

    +[UIResponder instanceFromNib] with recursive calling (WIP?)

---
+++ IRFoundations/UIResponder+IRAdditions.h
@@ -0,0 +1,15 @@
+//
+//  UIResponder+IRAdditions.h
+//  IRFoundations
+//
+//  Created by Evadne Wu on 3/2/12.
+//  Copyright (c) 2012 Iridia Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIResponder (IRAdditions)
+
++ (id) instanceFromNib;
+
+@end
