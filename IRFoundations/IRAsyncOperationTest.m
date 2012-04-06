00ba9d8c77fdfcafdacb056294074c9aa5d00334
Evadne Wu <ev@monoceroi.com>
2012-04-06 17:10:38 +0800

    finishes -[IRAsyncOperationTest testBarrierOperation]

--- IRFoundations/IRAsyncOperationTest.m
+++ IRFoundations/IRAsyncOperationTest.m
@@ -111,7 +111,55 @@
 
 - (void) testBarrierOperation {
 
-	STFail(@"Not implemented");
+	NSOperationQueue *queue = [[[NSOperationQueue alloc] init] autorelease];
+	[queue setMaxConcurrentOperationCount:1];
+	
+	NSMutableArray *operations = [NSMutableArray array];
+	IRAsyncBarrierOperation * (^operation)(BOOL, BOOL, BOOL, BOOL) = ^ (BOOL assertWorkerNotReached, BOOL throwFailure, BOOL assertFailure, BOOL assertSuccess) {
+	
+		__block IRAsyncBarrierOperation *operation = [[IRAsyncBarrierOperation operationWithWorkerBlock:^(IRAsyncOperationCallback callback) {
+		
+			NSUInteger ownIndex = [operations indexOfObject:operation];
+			STAssertFalse(ownIndex == NSNotFound, @"Operation %@ must be tracked by the test case");
+			
+			if (assertWorkerNotReached)
+				STFail(@"Worker block should not be reached in operation %@ (index %lu)", operation, (long)ownIndex);
+			
+			callback(throwFailure ? [NSError errorWithDomain:@"com.iridia.asyncOperation.mockError" code:0 userInfo:nil] : (id)kCFBooleanTrue);
+		
+		} completionBlock: ^ (id results) {
+		
+			if (assertFailure)
+				STAssertTrue([results isKindOfClass:[NSError class]], @"Operation should get an error");
+
+			if (assertSuccess)
+				STAssertFalse([results isKindOfClass:[NSError class]], @"Operation should not get an error");
+		
+		}] retain];
+		
+		IRAsyncOperation *lastOp = [operations lastObject];
+		if (lastOp)
+			[operation addDependency:lastOp];
+		
+		return operation;
+	
+	};
+	
+	//	Other operations bail after first simulated operation failed
+	//	If the operation comes after the failing operation, its worker block should not be reached
+	//	If the operation index is the failing one it should throw a failure
+	//	Any operation after the failing one, including the failing one, fails
+	//	Any operation before the failing one should not fail
+	
+	NSUInteger const failingOperationIndex = 25;
+	
+	for (int i = 0; i < 100; i++)
+		[operations addObject:operation((i > failingOperationIndex), (i == failingOperationIndex), (i >= failingOperationIndex), (i < failingOperationIndex))];
+
+	[queue addOperations:operations waitUntilFinished:NO];
+
+	while (queue.operationCount)
+		[[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:5]];
 
 }
 
