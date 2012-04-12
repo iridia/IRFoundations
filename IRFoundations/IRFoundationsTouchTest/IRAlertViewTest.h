4393505f59c3a8255b8ad6ddbc20eb9b9c0c08cd
Evadne Wu <ev@monoceroi.com>
2012-04-13 02:40:03 +0800

    some tests for the action sheet and the alert view

---
+++ IRFoundations/IRFoundationsTouchTest/IRAlertViewTest.h
@@ -0,0 +1,13 @@
+//
+//  IRAlertViewTest.h
+//  IRAlertViewTest
+//
+//  Created by Evadne Wu on 4/13/12.
+//  Copyright (c) 2012 Iridia Productions. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IRAlertViewTest : SenTestCase
+
+@end
